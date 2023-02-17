37bd3254be469bcce5a3cbdf6938d9b889129fca
Diogo Oliveira <diogo.oss29@gmail.com>
2023-02-16 21:20:30 -0300

    configurado minhas migrations

--- portifolio/database/migrations/2023_02_17_000246_create_portifolios_table.php
+++ portifolio/database/migrations/2023_02_17_000246_create_portifolios_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('portifolios', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title');
+            $table->string('description');
+            $table->string('type');
+            $table->string('url');
+            $table->string('patch');
         });
     }
 
